65adbf92e25d6e50c34f9c72b6b5842e234ab453
Jonatan Machado <jonatanaxe@outlook.com>
2024-09-29 11:47:37 -0300

    Initial commit v1.0.0

---
+++ src/registration.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ *  Copyright © Discorgento. All rights reserved.
+ */
+
+declare(strict_types=1);
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Discorgento_ProductLastOrderHyva', __DIR__);
